dca7116dafc8e1f0857c162c5aca367c15d52d87
antisandwichman <kpmeyer.two@gmail.com>
2022-02-17 15:06:36 -0500

    Easier to select which file you want to import

--- wtools/wtools.cpp
+++ wtools/wtools.cpp
@@ -4,24 +4,54 @@
 #include "word.h"
 #include "wordList.h"
 using namespace std;
-const string file = "wwordsalpha.txt";
+
 
 int main() {
+    string file = "";
+    bool notDoneYet = true;
+    enum FileList {
+        all5lwords = 1,
+        wordle = 2
+    };
     enum Option {
         search = 1,
         quit = 9
     };
-    fstream fiveLfile;
+
+    
+    cout << "Which word list would you like to import?" << endl;
+    cout << "1. All 5-Letter Words" << endl;
+    cout << "2. All Wordle Words" << endl;
+    int menuChoice;
+    cin >> menuChoice;
+    switch (menuChoice) {
+        case all5lwords: {
+            file = "5lwords.txt";
+            break;
+        }
+        case wordle: {
+            file = "wwordsalpha.txt";
+            break;
+        }
+        default: {
+            cout << "Invalid input." << endl;
+            return 0;
+            break;
+        }
+    }
+    
+
+    fstream incoming;
     wordList listOfWords;
-    fiveLfile.open(file, ios::in);
-    if (!fiveLfile) {
+    incoming.open(file, ios::in);
+    if (!incoming) {
         cout << "File not found";
     } else {
         cout << "File imported!" << endl;
         string cur;
         while (true) {
-            fiveLfile >> cur;
-            if (fiveLfile.eof()) {
+            incoming >> cur;
+            if (incoming.eof()) {
                 cout << "Word list populated." << endl;
                 break;
             }
@@ -32,7 +62,7 @@ int main() {
     }
 
     cout << "\033[2J\033[1;1H";
-    bool notDoneYet = true;
+    notDoneYet = true;
     do {
         cout << "ENTER ONE OF THE NUMBERS BELOW" << endl;
         cout << "------------------------------" << endl;
@@ -83,6 +113,6 @@ int main() {
                 break;
         }
     } while (notDoneYet);
-    fiveLfile.close();
+    incoming.close();
     return 0;
 }
